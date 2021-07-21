1ebe863b4fa943f79761bf6783f65ad7f39f9b6a
Ki Yung Ahn <kyagrd@gmail.com>
2021-07-21 09:49:20 +0900

    first note

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 HOSTPORT=8888
 
-docker run --rm -p $HOSTPORT:8888 -v $PWD:/home/jovyan/work --name ihaskell_notebook \
+# DOCKERRUN="docker run"
+DOCKERRUN="podman run --privileged --userns=keep-id"
+
+$DOCKERRUN --rm -p $HOSTPORT:8888 -v $PWD:/home/jovyan/work --name ihaskell_notebook \
 	crosscompass/ihaskell-notebook:latest jupyter lab --LabApp.token=''
 
